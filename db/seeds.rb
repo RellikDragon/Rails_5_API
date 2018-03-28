a3bc6658c28d1790515609b1b29d6102d9b5384e
Jared Dunn <rellikdragon@yahoo.com>
2018-03-28 12:32:54 -0600

    Reconfigured seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ FreelanceDocument.create!(
   title: "Document #{d}", 
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit",
-  image_url: "https://drive.google.com/open?id=1LxJhu5V7e8nFGgSiN2h1aLi5zUq0nJs9"
+  image_url: "https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg"
   )
 end
 
